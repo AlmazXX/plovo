af659432932a38877bfb586e10c41bf0602af776
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-23 01:28:54 +0600

    feat: Added onDelete()

--- src/features/Dish/Dishes.tsx
+++ src/features/Dish/Dishes.tsx
@@ -2,17 +2,29 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import Spinner from "../../components/Spinner/Spinner";
 import DishItem from "./components/DishItem";
-import { selectDishes, selectDishesFetching } from "./dishesSlice";
-import { fetchDishes } from "./dishesThunk";
+import {
+  selectDishDeleting,
+  selectDishes,
+  selectDishesFetching,
+} from "./dishesSlice";
+import { deleteDish, fetchDishes } from "./dishesThunk";
 
 const Dishes = () => {
   const dispatch = useAppDispatch();
   const dishes = useAppSelector(selectDishes);
   const fetching = useAppSelector(selectDishesFetching);
+  const deleting = useAppSelector(selectDishDeleting);
 
   useEffect(() => {
     dispatch(fetchDishes());
   }, [dispatch]);
+
+  const onDelete = async (id: string) => {
+    if (window.confirm("Do you really want to delete this dish?")) {
+      await dispatch(deleteDish(id));
+      await dispatch(fetchDishes());
+    }
+  };
 
   return (
     <>
@@ -20,7 +32,14 @@ const Dishes = () => {
       {fetching ? (
         <Spinner />
       ) : (
-        dishes.map((dish) => <DishItem key={dish.id} dish={dish} />)
+        dishes.map((dish) => (
+          <DishItem
+            key={dish.id}
+            dish={dish}
+            deleting={deleting === dish.id}
+            onDelete={() => onDelete(dish.id)}
+          />
+        ))
       )}
     </>
   );
