f89ea4a687e9256e463a107a2b5d2ef0aa22baae
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-22 23:27:38 +0600

    feat: Added Layout to App with React-Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+
 function App() {
-  return <></>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="*" element={<h1>Not found!</h1>} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
