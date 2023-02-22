3f478fa44c4836d167dde5613c23909b921a8fbf
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-22 23:59:57 +0600

    feat: Added createDish thunk to dishesSlice

--- src/features/Dish/dishesSlice.ts
+++ src/features/Dish/dishesSlice.ts
@@ -1,20 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
+import { createDish } from "./dishesThunk";
 
 interface DishesState {
   creating: boolean;
 }
 
 const initialState: DishesState = {
-  creating: false
-}
+  creating: false,
+};
 
 export const dishesSlice = createSlice({
   name: "dishes",
   initialState,
-  reducers: {}
-})
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(createDish.pending, (state) => {
+        state.creating = true;
+      })
+      .addCase(createDish.fulfilled, (state) => {
+        state.creating = false;
+      })
+      .addCase(createDish.rejected, (state) => {
+        state.creating = false;
+      });
+  },
+});
 
 export const dishesReducer = dishesSlice.reducer;
-export const selectDishCreating = (state: RootState) =>
-  state.dishes.creating;
+export const selectDishCreating = (state: RootState) => state.dishes.creating;
