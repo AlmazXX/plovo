939ae40f3e6c453bdaadeefd44899da04170915a
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-23 01:10:42 +0600

    feat: Added route update-dish

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import CreateDish from "./features/Dish/CreateDish";
+import UpdateDish from "./features/Dish/UpdateDish";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/create-dish" element={<CreateDish/>} />
+        <Route path="/create-dish" element={<CreateDish />} />
+        <Route path="/update-dish/:id" element={<UpdateDish />} />
         <Route path="*" element={<h1>Not found!</h1>} />
       </Routes>
     </Layout>
