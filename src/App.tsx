1dab3ecada2ae70cb908ab4c022dfb51e04c2dcb
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-23 02:20:25 +0600

    refactor: Added clientUrl ro routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import { clientUrl } from "./constants";
 import CreateDish from "./features/Dish/CreateDish";
 import UpdateDish from "./features/Dish/UpdateDish";
 import Home from "./features/Home/Home";
@@ -9,9 +10,9 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-dish" element={<CreateDish />} />
-        <Route path="/update-dish/:id" element={<UpdateDish />} />
+        <Route path={clientUrl} element={<Home />} />
+        <Route path={clientUrl + "/create-dish"} element={<CreateDish />} />
+        <Route path={clientUrl + "/update-dish/:id"} element={<UpdateDish />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
