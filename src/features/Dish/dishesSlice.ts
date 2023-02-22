ff9fc64c4db430571933f549c51c05eb93c540e6
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-23 00:32:54 +0600

    feat: Added fetchDishes() thunk to dishes slice

--- src/features/Dish/dishesSlice.ts
+++ src/features/Dish/dishesSlice.ts
@@ -1,13 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
-import { createDish } from "./dishesThunk";
+import { ApiDish } from "../../types";
+import { createDish, fetchDishes } from "./dishesThunk";
 
 interface DishesState {
+  items: ApiDish[];
   creating: boolean;
+  fetching: boolean;
 }
 
 const initialState: DishesState = {
+  items: [],
   creating: false,
+  fetching: false,
 };
 
 export const dishesSlice = createSlice({
@@ -16,6 +21,16 @@ export const dishesSlice = createSlice({
   reducers: {},
   extraReducers(builder) {
     builder
+      .addCase(fetchDishes.pending, (state) => {
+        state.fetching = true;
+      })
+      .addCase(fetchDishes.fulfilled, (state, { payload: dishes }) => {
+        state.fetching = false;
+        state.items = dishes;
+      })
+      .addCase(fetchDishes.rejected, (state) => {
+        state.fetching = false;
+      })
       .addCase(createDish.pending, (state) => {
         state.creating = true;
       })
@@ -29,4 +44,6 @@ export const dishesSlice = createSlice({
 });
 
 export const dishesReducer = dishesSlice.reducer;
+export const selectDishes = (state: RootState) => state.dishes.items;
 export const selectDishCreating = (state: RootState) => state.dishes.creating;
+export const selectDishesFetching = (state: RootState) => state.dishes.fetching;
