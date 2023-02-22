4040344d02d2d2a85f3c883cf1f5f095ddabcfde
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-22 23:25:33 +0600

    feat: Created Layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { FC, PropsWithChildren } from "react";
+import Navbar from "../Navbar/Navbar";
+
+const Layout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main className="container">{children}</main>
+    </>
+  );
+};
+
+export default Layout;
