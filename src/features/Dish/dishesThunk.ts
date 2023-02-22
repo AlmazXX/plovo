8aff1f931a18290b1bfc674b4dcc06feaef5cb4e
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-23 00:29:42 +0600

    feat: Added fetchDishes() thunk

--- src/features/Dish/dishesThunk.ts
+++ src/features/Dish/dishesThunk.ts
@@ -1,10 +1,21 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosApi from "../../axiosApi";
-import { Dish } from "../../types";
+import { ApiDish, ApiDishesList, Dish } from "../../types";
 
 export const createDish = createAsyncThunk(
   "dishes/create",
   async (dish: Dish) => {
     await axiosApi.post("/dishes.json", dish);
+  }
+);
+
+export const fetchDishes = createAsyncThunk<ApiDish[]>(
+  "/fetchDishes",
+  async () => {
+    const { data } = await axiosApi.get<ApiDishesList | null>("/dishes.json");
+    const dishes = data
+      ? Object.keys(data).map((id) => ({ ...data[id], id }))
+      : [];
+    return dishes;
   }
 );
