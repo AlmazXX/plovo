d9e58cb7add9f0cc3491eb429e12f78618c52890
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-23 00:14:19 +0600

    feat: Added Route to create dish page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import CreateDish from "./features/Dish/CreateDish";
 
 function App() {
   return (
     <Layout>
       <Routes>
+        <Route path="/create-dish" element={<CreateDish/>} />
         <Route path="*" element={<h1>Not found!</h1>} />
       </Routes>
     </Layout>
