c3fe55b1e9c91c597728a106765e373d9b4da6a1
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-23 00:36:39 +0600

    feat: Displayed fetched dishes

--- src/features/Dish/Dishes.tsx
+++ src/features/Dish/Dishes.tsx
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import Spinner from "../../components/Spinner/Spinner";
+import DishItem from "./components/DishItem";
+import { selectDishes, selectDishesFetching } from "./dishesSlice";
+import { fetchDishes } from "./dishesThunk";
+
 const Dishes = () => {
+  const dispatch = useAppDispatch();
+  const dishes = useAppSelector(selectDishes);
+  const fetching = useAppSelector(selectDishesFetching);
+
+  useEffect(() => {
+    dispatch(fetchDishes());
+  }, [dispatch]);
+
   return (
     <>
       <h4>Dishes</h4>
+      {fetching ? (
+        <Spinner />
+      ) : (
+        dishes.map((dish) => <DishItem key={dish.id} dish={dish} />)
+      )}
     </>
   );
 };
