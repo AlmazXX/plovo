ac748694cc8bf721b423a3bfa3810ebc8aa4e17e
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-22 23:24:56 +0600

    feat: Created Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar navbar-expand-sm navbar-dark bg-primary">
+      <div className="container-fluid">
+        <span className="navbar-brand">Plovo</span>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/new-dish" className="nav-link">
+                New dish
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/orders" className="nav-link">
+                Orders
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
